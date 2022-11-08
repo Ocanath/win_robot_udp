22e26a9ddbc57480d59c7a80dbd8b047481e8e42
ocanath <ocanath@gmail.com>
2022-11-08 14:54:50 -0800

    added code which gets the host ip address and binds the server socket to that address. it is also printed in the console. i do not yet know how to route the client to it automatically

--- udp_server/server.cpp
+++ udp_server/server.cpp
@@ -2,6 +2,7 @@
 	Simple UDP Server
 */
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 
 #include<stdio.h>
 #include<winsock2.h>
@@ -17,7 +18,8 @@ int main()
 	SOCKET s;
 	struct sockaddr_in server, si_other;
 	int slen, recv_len;
-	char buf[BUFLEN];
+	char r_buf[BUFLEN];	//receive buffer
+	char t_buf[BUFLEN];	//transmit buffer
 	WSADATA wsa;
 
 	slen = sizeof(si_other);
@@ -38,11 +40,25 @@ int main()
 	}
 	printf("Socket created.\n");
 	
+	char namebuf[256] = { 0 };
+	const char inet_addr_buf[256] = { 0 };
+	int rc = gethostname(namebuf, 256);
+	printf("hostname: %s\r\n", namebuf);
+	hostent* phost = gethostbyname(namebuf);
+	for (int i = 0; phost->h_addr_list[i] != NULL; i++)
+	{
+		PCSTR retv = inet_ntop(AF_INET, phost->h_addr_list[i], (PSTR)inet_addr_buf, 256);
+		printf("address %d: %s\r\n", i, inet_addr_buf);
+	}
+
 	//Prepare the sockaddr_in structure
 	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = inet_addr("192.168.29.220");
+	server.sin_addr.s_addr = inet_addr(inet_addr_buf);//inet_addr("192.168.29.220");
 	server.sin_port = htons(PORT);
 
+	inet_ntop(AF_INET, &server.sin_addr.s_addr, (PSTR)inet_addr_buf, 256);	//convert again the value we copied thru and display
+	printf("Binding to server address: %s\r\n", inet_addr_buf);
+	
 	//Bind
 	if (bind(s, (struct sockaddr*)&server, sizeof(server)) == SOCKET_ERROR)
 	{
@@ -58,23 +74,34 @@ int main()
 		fflush(stdout);
 
 		//clear the buffer by filling null, it might have previously received data
-		memset(buf, '\0', BUFLEN);
+		memset(r_buf, '\0', BUFLEN);
 
 		//try to receive some data, this is a blocking call
-		if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr*)&si_other, &slen)) == SOCKET_ERROR)
+		if ((recv_len = recvfrom(s, r_buf, BUFLEN, 0, (struct sockaddr*)&si_other, &slen)) == SOCKET_ERROR)
 		{
 			printf("recvfrom() failed with error code : %d", WSAGetLastError());
 			//exit(EXIT_FAILURE);
 		}
 
 		//print details of the client/peer and the data received
-		const char inet_addr_buf[256] = { 0 };
+		
 		PCSTR retv = inet_ntop(AF_INET, &si_other.sin_addr, (PSTR)inet_addr_buf, 256);
 		printf("Received packet from %s:%d\n", inet_addr_buf, ntohs(si_other.sin_port));
-		printf("Data: %s\n", buf);
+		printf("Data: %s\n", r_buf);
+
+
+		memset(t_buf, '\0', BUFLEN);
+		sprintf(t_buf, "why did you send me ");
+		int hdr_len = strlen(t_buf);
+		int cpy_idx = 0;
+		for (int i = hdr_len; (r_buf[cpy_idx] != 0) && (i < BUFLEN); i++)
+		{
+			t_buf[i] = r_buf[cpy_idx++];
+		}
+		int t_len = strlen(t_buf);
 
 		//now reply the client with the same data
-		if (sendto(s, buf, recv_len, 0, (struct sockaddr*)&si_other, slen) == SOCKET_ERROR)
+		if (sendto(s, t_buf, t_len, 0, (struct sockaddr*)&si_other, slen) == SOCKET_ERROR)
 		{
 			printf("sendto() failed with error code : %d", WSAGetLastError());
 			//exit(EXIT_FAILURE);
